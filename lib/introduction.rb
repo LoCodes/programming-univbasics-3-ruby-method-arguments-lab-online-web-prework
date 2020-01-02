fd8b8fc659604b4a3dea17611c94f22a4a77ac79
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-02 00:21:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end 
 
-def introduction_with_language('Loren' , 'Ruby')
+def introduction_with_language("Josh", "Ember.js")
   puts "Hi, my name is #{name} and I am learning to program #{language}."
 end
 
